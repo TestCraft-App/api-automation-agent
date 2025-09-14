b797697f2ae425e54dd92bbaff18ef0554d1f6c7
asolisgithub <ayrtonsolis153@hotmail.com>
2025-09-14 19:51:31 -0300

    fixed typo

--- src/ai_tools/models/model_fix_input.py
+++ src/ai_tools/models/model_fix_input.py
@@ -6,7 +6,7 @@ class ModelFixInput(ModelCreationInput):
     changes: str = Field(
         description="A string describing the changes needed to fix issues in the provided files. Phrased it in 'Do not do X. Solution is Y'",
         examples=[
-            "Do not introduce not-user var errors, include in the file only what you will use.",
+            "Do not introduce not-used var errors, include in the file only what you will use.",
             "Do not access Arrays without checking for null or undefined indexes first. Guard against out-of-bounds errors using if(falsy) throw new Error('...')",
             "Do not use non-null assertion operator (!). Properly handle null and undefined values using conditional checks.",
         ],
