bfb209f9ea5b00f1f8dbbabf7431778cc57ddf1c
asolisgithub <ayrtonsolis153@hotmail.com>
2025-10-10 16:57:44 -0300

    removed for

--- src/ai_tools/models/test_fix_input.py
+++ src/ai_tools/models/test_fix_input.py
@@ -30,7 +30,7 @@ class TestFixInput(FileCreationInput):
         description="A string describing the changes needed to fix issues in the provided files. Phrased it in 'Do not do X. Solution is Y'",
         examples=[
             "Do not use larger than 32 chars names for firstName. Use short, unique names instead.",
-            "Don't assert status for for order deletion with status 204. Expect 200 instead.",
+            "Don't assert status for order deletion with status 204. Expect 200 instead.",
             "Don't expect a string in the 'Create new Order' Test, use AddOrderReponse[] as the response generic type and expect a single item array.",
         ],
     )
